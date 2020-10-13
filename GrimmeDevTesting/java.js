59e5d6513f2fa3138553e9b45bd14e3645b3aad7
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-13 07:08:59 -0500

    got images working

--- GrimmeDevTesting/java.js
+++ GrimmeDevTesting/java.js
@@ -1,5 +1,7 @@
 // timer to automatically cycle through x images
-var images = ["0A0Q1pD", "0rGZQud", "0VWuvmu", "1CuFckW", "1pDN9Gx", "2BpaHvh"];
+var images = ["0A0Q1pD.jpg", "0rGZQud.jpg", "0VWuvmu.jpg", "1CuFckW.jpg", "1pDN9Gx.jpg", "2BpaHvh.jpg"];
+
+
 
 var counter = 0;
 var max = images.length;
@@ -8,21 +10,21 @@ var max = images.length;
 
 
 function timer() {
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
         console.log(counter);
         if (counter != max) {
             // clear previous image
-            $(".imageDiv").empty();
+            $("#image-div").empty();
             // display new image
             var element = (`
-            <img src="./Unsolved/images/${images[counter]}" alt="${images[counter]}"/>            
+            <img src="./GrimmeDevTesting/images/${images[counter]}" alt="${images[counter]}"/>            
             `);
             // var element = $("<img>");
             // element.attr("src", `./Unsolved/images/${images[counter]}`);
             // element.attr("alt", `${images[counter]}`);
             console.log(element);
             console.log(images[counter]);
-            $(".imageDiv").append(element);
+            $("#image-div").append(element);
             counter++;
         }
         // if image index is at max, stop timer
